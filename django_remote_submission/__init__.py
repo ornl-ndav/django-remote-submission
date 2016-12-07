f548769c7ae61652f4cbfcb96dabb96252d59619
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2016-12-07 15:52:32 -0500

    Logs work on travis but not locally. Without set pragmas.

--- django_remote_submission/__init__.py
+++ django_remote_submission/__init__.py
@@ -1,20 +1,3 @@
-from django.db.backends.signals import connection_created
-from django.db import connections
-
 __version__ = '0.2.0'
 
 default_app_config = 'django_remote_submission.apps.DjangoRemoteSubmissionConfig'
-
-def activate_pragmas(sender, connection, **kwargs):
-    """
-    Runs PRAGMA commands in SQLite
-    """
-    if connection.vendor == 'sqlite':
-        connections['default'].allow_thread_sharing = True
-        cursor = connection.cursor()
-        cursor.execute('PRAGMA busy_timeout = 30000;')
-        cursor.execute('PRAGMA journal_mode=WAL;')
-        cursor.execute("PRAGMA main.wal_checkpoint(PASSIVE);")
-
-connection_created.connect(activate_pragmas)
-
