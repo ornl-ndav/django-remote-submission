3d4149a01b946fb8bf94ab0a0d146b36b20cdb83
Tanner Hobson <thobson125@gmail.com>
2016-12-19 17:01:42 -0500

    Refactor to use a wrapper around Paramiko (WIP)

---
+++ django_remote_submission/remote.py
@@ -0,0 +1,162 @@
+"""
+
+"""
+
+from __future__ import absolute_import, unicode_literals, print_function
+import shlex
+import logging
+import os
+import textwrap
+import datetime
+
+from paramiko import (
+    AuthenticationException, BadHostKeyException,
+)
+from paramiko.client import SSHClient, AutoAddPolicy
+from django.utils.timezone import now
+
+logger = logging.getLogger(__name__)
+
+
+class LogPolicy(object):
+    LOG_NONE = 0
+    LOG_LIVE = 1
+    LOG_TOTAL = 2
+
+
+class RemoteWrapper(object):
+    """
+
+    """
+    def __init__(self, hostname, username, port=22):
+        self.hostname = hostname
+        self.username = username
+        self.port = port
+        self._client = None
+        self._sftp = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def connect(self, password=None, public_key_filename=None):
+        self._client = self._start_client(password, public_key_filename)
+        self._sftp = self._client.open_sftp()
+        return self
+
+    def close(self):
+        self._sftp.close()
+        self._sftp = None
+
+        self._client.close()
+        self._client = None
+
+    def chdir(self, remote_directory):
+        self._sftp.chdir(remote_directory)
+
+    def open(self, filename, mode):
+        return self._sftp.open(filename, mode)
+
+    def listdir_attr(self):
+        return self._sftp.listdir_attr()
+
+    def exec_command(self, args, timeout=None, stdout_handler=None,
+                     stderr_handler=None):
+        command = self._make_command(args, timeout)
+
+        transport = self._client.get_transport()
+        channel = transport.open_session()
+
+        channel.exec_command(command)
+        while True:
+            current_time = now()
+            if channel.recv_ready():
+                output = channel.recv(1024).decode('utf-8')
+                stdout_handler(current_time, output)
+
+            if channel.recv_stderr_ready():
+                output = channel.recv_stderr(1024).decode('utf-8')
+                stderr_handler(current_time, output)
+
+            if channel.exit_status_ready():
+                if channel.recv_exit_status() == 0:
+                    return True
+                else:
+                    return False
+
+    def _start_client(self, password, public_key_filename):
+        if public_key_filename is None:
+            public_key_filename = os.path.expanduser('~/.ssh/id_rsa.pub')
+
+        client = SSHClient()
+        client.set_missing_host_key_policy(AutoAddPolicy())
+
+        server_hostname = self.hostname
+        server_port = self.port
+
+        try:
+            logger.info("Connecting to %s with public key.", server_hostname)
+            client.connect(
+                server_hostname,
+                port=server_port,
+                username=self.username,
+                key_filename=public_key_filename,
+            )
+
+        except (AuthenticationException, BadHostKeyException) as e:
+            if password is None:
+                logger.error("Connection with public key failed! "
+                             "The password is mandatory")
+                raise ValueError('needs password') from e
+
+            try:
+                logger.info("Connecting to %s with password.", server_hostname)
+                client.connect(
+                    server_hostname,
+                    port=server_port,
+                    username=self.username,
+                    password=password,
+                )
+                deploy_key_if_it_doesnt_exist(client, public_key_filename)
+
+            except AuthenticationException as e:
+                logger.error("Authenctication error! Wrong password...")
+                raise ValueError('incorrect password') from e
+
+            else:
+                return client
+
+    def _make_command(self, args, timeout):
+        command = ' '.join(shlex.quote(arg) for arg in args)
+
+        if timeout is not None:
+            command = 'timeout {}s {}'.format(timeout.total_seconds(), command)
+
+        return command
+
+
+def deploy_key_if_it_doesnt_exist(client, public_key_filename):
+    """
+
+    """
+    with open(public_key_filename, 'rt', encoding='utf-8') as f:
+        key = f.read()
+
+    client.exec_command('mkdir -p ~/.ssh/')
+
+    command = textwrap.dedent('''\
+    KEY={}
+    if [ -z "$(grep \"$KEY\" ~/.ssh/authorized_keys )" ]; then
+        printf $'%s\n' "$KEY" >> ~/.ssh/authorized_keys
+        echo key added.
+    fi
+    '''.format(shlex.quote(key)))
+
+    stdin, stdout, stderr = client.exec_command(command)
+    logger.debug(stdout.readlines())
+    logger.debug(stderr.readlines())
+
+    client.exec_command('chmod 644 ~/.ssh/authorized_keys')
+    client.exec_command('chmod 700 ~/.ssh/')
--- django_remote_submission/tasks.py
+++ django_remote_submission/tasks.py
@@ -21,6 +21,7 @@ from threading import Thread
 from django.core.files import File
 
 from .models import Log, Job, Result, Interpreter
+from .remote import RemoteWrapper
 
 try:
     from celery import shared_task
@@ -42,148 +43,22 @@ class LogPolicy(object):
     LOG_TOTAL = 2
 
 
-class RemoteRunner(object):
-    """
-    Example usage:
+def is_matching(filename, patterns=None):
+    if patterns is None:
+        patterns = ['*']
 
-    >>> with RemoteRunner(job, 'password0') as runner:
-    ...     runner.copy_program()
-    ...     exit_status = runner.run_program(
-    ...         stdout_handler=lambda now, output, job: print('stdout', output),
-    ...         stderr_handler=lambda now, output, job: print('stderr', output),
-    ...     )
-    ...     runner.get_modified_files(
-    ...         file_handler=lambda filename, f, job: print('changed', filename),
-    ...     )
+    is_matching = False
 
-    """
-    SUCCESS = object()
-    FAILURE = object()
+    for pattern in patterns:
+        if not pattern.startswith('!'):
+            if fnmatch.fnmatch(filename, pattern):
+                is_matching = True
+        else:
+            if fnmatch.fnmatch(filename, pattern[1:]):
+                is_matching = False
 
-    def __init__(self, job, password, username=None):
-        self.client = start_client(job, username, password)
-        self.sftp = self.client.open_sftp()
-        self.job = job
+    return is_matching
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
-
-    def close(self):
-        self.sftp.close()
-        self.client.close()
-
-    def copy_program(self, program):
-        self.sftp.chdir(job.remote_directory)
-        self.sftp.putfo(six.StringIO(job.program), job.remote_filename)
-
-    def run_program(self, timeout=None, stdout_handler=None,
-                    stderr_handler=None):
-        command = self._format_command(timeout)
-
-        transport = self.client.get_transport()
-        channel = transport.open_session()
-
-        channel.exec_command(command)
-        while True:
-            now = datetime.datetime.now()
-            if channel.recv_ready():
-                output = channel.recv(1024).decode('utf-8')
-                stdout_handler(now, output, job)
-
-            if channel.recv_stderr_ready():
-                output = channel.recv_stderr(1024).decode('utf-8')
-                stderr_handler(now, output, job)
-
-            if channel.exit_status_ready():
-                if channel.recv_exit_status() == 0:
-                    return RemoteRunner.SUCCESS
-                else:
-                    return RemoteRunner.FAILURE
-
-
-    def get_modified_files(self, file_handler=None):
-        file_attrs = self.sftp.listdir_attr()
-        file_map = { attr.filename: attr for attr in file_attrs }
-        script_attr = file_map[job.remote_filename]
-        script_mtime = script_attr.st_mtime
-
-        result_set = ResultSet(patterns=store_results)
-        results = []
-        for attr in file_attrs:
-            if attr is script_attr:
-                continue
-
-            if attr.st_mtime < script_mtime:
-                continue
-
-            if not result_set.matches(attr.filename):
-                continue
-
-            with sftp.open(attr.filename, 'rb') as f:
-                file_handler(attr.filename, f, job)
-
-    def _format_command(self, timeout=None):
-        command = '{} {}'.format(self.job.interpreter.path, job.remote_filename)
-        if timeout is not None:
-            command = 'timeout {}s {}'.format(timeout.total_seconds(), command)
-        command='cd {} && {}'.format(job.remote_directory, command)
-
-        return command
-
-
-class ResultSet(object):
-    def __init__(self, patterns):
-        if patterns is None:
-            patterns = ['*']
-
-        self.patterns = patterns
-
-    def filter(self, filenames):
-        return [
-            x
-            for x in filenames
-            if self.matches(x)
-        ]
-
-    def matches(self, filename):
-        is_matching = False
-
-        for pattern in self.patterns:
-            if not pattern.startswith('!'):
-                if fnmatch.fnmatch(filename, pattern):
-                    is_matching = True
-            else:
-                if fnmatch.fnmatch(filename, pattern[1:]):
-                    is_matching = False
-
-        return is_matching
-
-
-def deploy_key_if_it_doesnt_exist(client, public_key_filename):
-    '''
-
-    '''
-    key = open(public_key_filename).read()
-    client.exec_command('mkdir -p ~/.ssh/')
-
-    command = '''
-    KEY="%s"
-    if [ -z "$(grep \"$KEY\" ~/.ssh/authorized_keys )" ];
-    then
-        echo $KEY >> ~/.ssh/authorized_keys;
-        echo key added.;
-    fi;
-    '''%key
-
-    stdin, stdout, stderr  = client.exec_command(command)
-    logger.debug(stdout.readlines())
-    logger.debug(stderr.readlines())
-
-    client.exec_command('chmod 644 ~/.ssh/authorized_keys')
-    client.exec_command('chmod 700 ~/.ssh/')
 
 all_lines = {
     'stdout' : [],
@@ -221,101 +96,119 @@ def store_logs(stream, stream_type, log_policy, job):
                 raise ValueError(msg)
 
 
-@shared_task
-def submit_job_to_server(job_pk, password, username=None, client=None,
-                         log_policy=LogPolicy.LOG_LIVE, timeout=None,
-                         store_results=None):
-    '''
-    TODO: Refactoring!!
-    '''
+class LogContainer(object):
+    LogLine = collections.namedtuple('LogLine', [
+        'now', 'output',
+    ])
 
+    def __init__(self, job, log_policy):
+        self.job = job
+        self.log_policy = log_policy
+        self.stdout = []
+        self.stderr = []
+
+    def write(self, lst, now, output):
+        lst.append(LogContainer.LogLine(
+            now=now,
+            output=output,
+        ))
+
+        if self.log_policy == LogPolicy.LOG_LIVE:
+            self.flush()
+
+    def write_stdout(self, now, output):
+        self.write(self.stdout, now, output)
+
+    def write_stderr(self, now, output):
+        self.write(self.stderr, now, output)
+
+    def flush(self):
+        print('flush: {!r} {!r}'.format(self.stdout, self.stderr))
+
+        if len(self.stdout) > 0:
+            Log.objects.create(
+                time=self.stdout[-1],
+                content='\n'.join(line.output for line in self.stdout),
+                stream='stdout',
+                job=self.job,
+            )
+
+            del self.stdout[:]
+
+        if len(self.stderr) > 0:
+            Log.objects.create(
+                time=self.stderr[-1].now,
+                content='\n'.join(line.output for line in self.stderr),
+                stream='stderr',
+                job=self.job,
+            )
+
+            del self.stderr[:]
+
+
+@shared_task
+def submit_job_to_server(job_pk, password, username=None, timeout=None,
+                         log_policy=LogPolicy.LOG_LIVE, store_results=None):
     job = Job.objects.get(pk=job_pk)
 
     if username is None:
         username = job.owner.username
 
-    if client is None:
-        client = start_client(job, username, password)
+    wrapper = RemoteWrapper(
+        hostname=job.server.hostname,
+        username=username,
+        port=job.server.port,
+    )
 
-    sftp = client.open_sftp()
-    sftp.chdir(job.remote_directory)
-    sftp.putfo(six.StringIO(job.program), job.remote_filename)
+    logs = LogContainer(
+        job=job,
+        log_policy=log_policy,
+    )
 
-    job.status = Job.STATUS.submitted
-    job.save()
+    with wrapper.connect(password):
+        wrapper.chdir(job.remote_directory)
 
-    command = '{} {}'.format(job.interpreter.path,job.remote_filename)
-    if timeout is not None:
-        command = 'timeout {}s {}'.format(timeout.total_seconds(), command)
-    command='mkdir -p {0} && cd {0} && {1}'.format(job.remote_directory,command)
+        with wrapper.open(job.remote_filename, 'wt') as f:
+            f.write(job.program)
 
-    transport = client.get_transport()
-    channel = transport.open_session()
+        job.status = Job.STATUS.submitted
+        job.save()
 
-    logger.debug("Executing remotely the command: %s.", command)
-    channel.exec_command(command)
-    logger.debug("Executed...")
-    done = False
-    while not done:
-        if channel.recv_ready():
-            stream = channel.recv(1024).decode('utf-8')
-            store_logs(stream,'stdout', log_policy, job)
-        if channel.recv_stderr_ready():
-            stream = channel.recv_stderr(1024).decode('utf-8')
-            store_logs(stream,'stderr', log_policy, job)
-        if channel.exit_status_ready():
-            if channel.recv_exit_status() == 0:
-                job.status = Job.STATUS.success
-            else:
-                job.status = Job.STATUS.failure
-            job.save()
-            done = True
-
-    global all_lines
-    if log_policy == LogPolicy.LOG_TOTAL:
-        if all_lines['stdout']:
-            Log.objects.create(
-                content='\n'.join(all_lines['stdout']),
-                stream='stdout',
-                job=job,
-            )
-            all_lines['stdout'] = []
-        if all_lines['stderr']:
-            Log.objects.create(
-                content='\n'.join(all_lines['stderr']),
-                stream='stderr',
-                job=job,
-            )
-            all_lines['stderr'] = []
-
-    file_attrs = sftp.listdir_attr()
-    file_map = { attr.filename: attr for attr in file_attrs }
-    script_attr = file_map[job.remote_filename]
-    script_mtime = script_attr.st_mtime
-
-    result_set = ResultSet(patterns=store_results)
-    results = []
-    for attr in file_attrs:
-        if attr is script_attr:
-            continue
-
-        if attr.st_mtime < script_mtime:
-            continue
-
-        if not result_set.matches(attr.filename):
-            continue
-
-        result = Result.objects.create(
-            remote_filename=attr.filename,
-            job=job,
+        job_status = wrapper.exec_command(
+            [job.interpreter.path, job.remote_filename],
+            timeout=timeout,
+            stdout_handler=logs.write_stdout,
+            stderr_handler=logs.write_stderr,
         )
 
-        with sftp.open(attr.filename, 'rb') as f:
-            result.local_file.save(attr.filename, File(f), save=True)
+        logs.flush()
 
-        results.append(result)
+        file_attrs = wrapper.listdir_attr()
+        file_map = { attr.filename: attr for attr in file_attrs }
+        script_attr = file_map[job.remote_filename]
+        script_mtime = script_attr.st_mtime
 
-    client.close()
+        results = []
+        for attr in file_attrs:
+            if attr is script_attr:
+                continue
+
+            if attr.st_mtime < script_mtime:
+                continue
+
+            if not is_matching(attr.filename, store_results):
+                continue
+
+            result = Result.objects.create(
+                remote_filename=attr.filename,
+                job=job,
+            )
+
+            with wrapper.open(attr.filename, 'rb') as f:
+                result.local_file.save(attr.filename, File(f), save=True)
+
+            results.append(result)
+
     return results
 
 
