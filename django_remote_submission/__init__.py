42cf308fc8940532d31365786ca620d1550d3bef
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2018-04-26 11:39:21 -0400

    Bump version: 1.1.6 → 1.1.7

--- django_remote_submission/__init__.py
+++ django_remote_submission/__init__.py
@@ -11,6 +11,6 @@ the models to make this function work.
 
 """
 
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 
 default_app_config = 'django_remote_submission.apps.DjangoRemoteSubmissionConfig'
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = '1.1.6'
+version = '1.1.7'
 
 if sys.argv[-1] == 'publish':
     try:
@@ -56,7 +56,7 @@ setup(
         # Celery
         'celery~=4.1',
         # Add websocket connections to Django
-        'channels~=1.1.6',
+        'channels~=1.1.7',
     ],
     # install_requires=reqs,
     license="ISCL",
